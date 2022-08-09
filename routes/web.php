15d7ed80bf99b274a6ef42b1f5a414a44c446b18
shoaib uddin <shoaibuddin12fx@gmail.com>
2022-08-09 08:07:27 +0500

    voya

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use TCG\Voyager\Voyager;
+use TCG\Voyager\Facades\Voyager;
 
 /*
 |--------------------------------------------------------------------------
